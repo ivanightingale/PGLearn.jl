c08ddbf14402bacc333133a7673258b3025b8628
Ivan <gqiu7@gatech.edu>
2025-09-10 14:56:32 -0400

    Update cholesky_plots.jl and vis_utils.jl

--- exp/vis/cholesky_plots.jl
+++ exp/vis/cholesky_plots.jl
@@ -17,17 +17,20 @@ using PGLearn
 include("vis_utils.jl")
 
 case_name = "14_ieee"
-file_path = joinpath("data", case_name, "$(pred_name).h5")
-
-res_vec = JLD2.load("cholesky_$(case_name).jld2", "data")
-seeds = map(res -> res["input"]["seed"], res_vec)
-load_sums = map(res -> sum(res["input"]["pd"] .+ res["input"]["qd"]), res_vec)
-sorted_load_sums = sort(load_sums)
-load_sums_perm = sortperm(load_sums)
-pred_triu_dict = load_dict_hdf5(file_path)
 
 network = make_basic_network(pglib(case_name))
 groups = Set.(PGLearn.OPFData(network, compute_clique_decomposition=true).clique_decomposition)
+
+res_vec = JLD2.load("cholesky_$(case_name).jld2", "data")
+load_sums, trius = reconstruct_trius_dict(res_vec, groups)
+trius_pred = nothing
+
+# n_instances = 100
+# pred_name = "nu_polar_polar_0.001_2_512_512_512"
+# file_path = joinpath("data", case_name, "$(pred_name).h5")
+# load_sums, trius, trius_pred = load_hdf5_to_dict(file_path; n_instances)
+
+groups = filter_groups(trius; k=40)
 
 n_rows = ceil(Int, sqrt(length(groups)))
 n_cols = ceil(Int, length(groups) / n_rows)
@@ -36,8 +39,8 @@ legend_size = 200
 fig = Figure(size=(row_size * n_cols, row_size * n_rows + legend_size))
 
 palette = colorschemes[:tab10]
-global axes = []
-global sample_ax = nothing
+global axs = []
+global sample_ax = nothing  # for adding legends
 markersize = 4
 for (plt_i, group) in enumerate(groups)
     n = length(group)
@@ -52,75 +55,80 @@ for (plt_i, group) in enumerate(groups)
         ax = Axis(group_grid[i, i])
         scatter!(
             ax,
-            sorted_load_sums,
-            [real(res["dual"]["cholesky"][group].U[i, i]) for res in res_vec][load_sums_perm];
+            load_sums,
+            real(trius[group][i, i, :]);
             markersize=markersize,
             color=true_re_color
         )
         scatter!(
             ax,
-            sorted_load_sums,
-            [imag(res["dual"]["cholesky"][group].U[i, i]) for res in res_vec][load_sums_perm];
+            load_sums,
+            imag(trius[group][i, i, :]);
             markersize=markersize,
             color=true_im_color
         )
-        scatter!(
-            ax,
-            sorted_load_sums,
-            real(pred_triu_dict[group][i, i, eachindex(seeds)])[load_sums_perm];
-            markersize=markersize,
-            color=pred_re_color
-        )
-        scatter!(
-            ax,
-            sorted_load_sums,
-            imag(pred_triu_dict[group][i, i, eachindex(seeds)])[load_sums_perm];
-            markersize=markersize,
-            color=pred_im_color
-        )
-        push!(axes, ax)
+        if !isnothing(trius_pred)
+            scatter!(
+                ax,
+                load_sums,
+                real(trius_pred[group][i, i, :]);
+                markersize=markersize,
+                color=pred_re_color
+            )
+            scatter!(
+                ax,
+                load_sums,
+                imag(trius_pred[group][i, i, :]);
+                markersize=markersize,
+                color=pred_im_color
+            )
+        end
+        push!(axs, ax)
         for j in i+1:n
             ax = Axis(group_grid[i, j])
             isnothing(sample_ax) && (global sample_ax = ax)
             scatter!(
                 ax,
-                sorted_load_sums,
-                [real(res["dual"]["cholesky"][group].U[i, j]) for res in res_vec][load_sums_perm];
+                load_sums,
+                real(trius[group][i, j, :]);
                 markersize=markersize,
                 color=true_re_color,
                 label="Re"
             )
             scatter!(
                 ax,
-                sorted_load_sums,
-                [imag(res["dual"]["cholesky"][group].U[i, j]) for res in res_vec][load_sums_perm];
+                load_sums,
+                imag(trius[group][i, j, :]);
                 markersize=markersize,
                 color=true_im_color,
                 label="Im"
             )
-            scatter!(
-                ax,
-                sorted_load_sums,
-                real(pred_triu_dict[group][i, j, eachindex(seeds)])[load_sums_perm];
-                markersize=markersize,
-                color=pred_re_color,
-                label="Re - $(pred_name)"
-            )
-            scatter!(
-                ax,
-                sorted_load_sums,
-                imag(pred_triu_dict[group][i, j, eachindex(seeds)])[load_sums_perm];
-                markersize=markersize,
-                color=pred_im_color,
-                label="Im - $(pred_name)"
-            )
-            push!(axes, ax)
+            if !isnothing(trius_pred)
+                scatter!(
+                    ax,
+                    load_sums,
+                    real(trius_pred[group][i, j, :]);
+                    markersize=markersize,
+                    color=pred_re_color,
+                    label="Re - $(pred_name)"
+                )
+                scatter!(
+                    ax,
+                    load_sums,
+                    imag(trius_pred[group][i, j, :]);
+                    markersize=markersize,
+                    color=pred_im_color,
+                    label="Im - $(pred_name)"
+                )
+            end
+            push!(axs, ax)
         end
     end
     Label(group_grid[0, :], string(group), fontsize = 20)
-    linkyaxes!(axes...)
-    global axes = []
+    linkyaxes!(axs...)
+    global axs = []
 end
+# linkyaxes!(axs...)
 
 Legend(
     fig[end+1, :],
@@ -130,4 +138,5 @@ Legend(
 )
 fig.layout.rowsizes = vcat(repeat([Relative(row_size / (n_rows * row_size + legend_size))]; outer=n_rows), [Relative(legend_size / (n_rows * row_size + legend_size))])
 
+# save("cholesky_plots_$(case_name)_$(pred_name).png", fig)
 save("cholesky_plots_$(case_name).png", fig)
--- exp/vis/vis_utils.jl
+++ exp/vis/vis_utils.jl
@@ -1,5 +1,6 @@
 using HDF5
 using Colors
+using Statistics:std
 
 brighten(c::RGB, α=0.6) = RGB(
     clamp(c.r + α*(1 - c.r), 0, 1),
@@ -7,15 +8,54 @@ brighten(c::RGB, α=0.6) = RGB(
     clamp(c.b + α*(1 - c.b), 0, 1)
 )
 
-function load_dict_hdf5(filename)
+function reconstruct_trius_dict(trius_flat::Matrix, groups::Vector)
+    # convert ((2 * total_trius_dim) × n_instances) flat trius data to Dict(clique => (n_c × n_c × n_instances) Array)
+    n_instances = size(trius_flat, 2)
     d = Dict()
-    h5open(filename, "r") do file
-        # for each key which corresponds to a clique
-        for key in keys(file)
-            # convert string to Tuple then to Set, convert to Julia index
-            parsed_key = ((key |> Meta.parse |> eval |> collect) .+ 1) |> Set
-            d[parsed_key] = read(file[key])  # n_clique × n_clique × n_data
+    trius_r = trius_flat[1 : div(size(trius_flat, 1), 2), :]
+    trius_i = trius_flat[div(size(trius_flat, 1), 2) + 1 : end, :]
+    trius = trius_r + im .* trius_i  # total_trius_dim × n_instances
+    trius_ptr = 1
+    for c in groups
+        n_c = length(c)
+        triu_dim_c = div((n_c + 1) * n_c, 2)
+        triu_mat = zeros(Complex{eltype(trius_flat)}, n_c, n_c, n_instances)
+        # place the values into the matrix in row-major order
+        for i in axes(triu_mat, 1)
+            for j in i:size(triu_mat, 2)
+                triu_mat[i, j, :] = trius[trius_ptr, :]
+                trius_ptr += 1
+            end
         end
+        d[c] = triu_mat
     end
     return d
+end
+
+function reconstruct_trius_dict(res_vec::Vector, groups::Vector)
+    # extract Dict(clique => (n_c × n_c × n_instances) Array) from a Vector of Dict of solutions
+    res_vec_solved = filter((res) -> res["meta"]["termination_status"] in ["OPTIMAL", "SLOW_PROGRESS"], res_vec)
+    triu_flat_vec = [res["dual"]["trius"] for res in res_vec_solved]  # Vector of Vectors with length (2 * total_trius_dim)
+    trius_flat = reduce(hcat, triu_flat_vec)  # n_c × n_c × n_solved_instances
+    load_sums = map(res -> sum(res["input"]["pd"] .+ res["input"]["qd"]), res_vec_solved)
+    return load_sums, reconstruct_trius_dict(trius_flat, groups)
+end
+
+function filter_groups(trius::Dict; k=30)
+    # if the number of cliques is too large, keep only the top k cliques with the greatest maximum standard deviation in its data
+    # For each clique, compute the standard deviation of each entry across the instances and take the maximum
+    return partialsort(collect(keys(trius)), 1:k; by=c -> maximum(std(trius[c], dims=3)), rev=true)
+end
+
+function load_hdf5_to_dict(file_path::String; n_instances=100)
+    return h5open(file_path, "r") do file
+        x_dataset = file["x"]
+        trius_dataset = file["nu_conic"]["trius"]
+        trius_pred_dataset = file["nu_conic_pred"]["trius"]
+        instances_slice = 1:min(n_instances, size(trius_dataset, 2))
+        load_sums = vec(sum(x_dataset["pd"][:, instances_slice] + x_dataset["qd"][:, instances_slice], dims=1))
+        trius = trius_dataset[:, instances_slice]
+        trius_pred = trius_pred_dataset[:, instances_slice]
+        load_sums, reconstruct_trius_dict(trius, groups), reconstruct_trius_dict(trius_pred, groups)
+    end
 end
